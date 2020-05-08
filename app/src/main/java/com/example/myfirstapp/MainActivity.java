add7eeb3e67f84b895a9da1d472546e5586f7550
kazuhitonakayama <firstjin19990101@gmail.com>
2020-05-08 18:13:28 +0900

    _80_小数点第一位のみ表示しよう_String.format(%.1f,bmi)

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton(android.view.View view) {
+        TextView bmiScore = (TextView)this.findViewById(R.id.bmiScore);
+        EditText heightForm = (EditText)this.findViewById(R.id.heightForm);
+        EditText weightForm = (EditText)this.findViewById(R.id.weightForm);
+        // ここで多分どこの何を参照するかを定義してる
+
+        String strHeight,strWeight;
+        strHeight = heightForm.getText().toString();
+        strWeight = weightForm.getText().toString();
+        //　んで実際にそれぞれの入力内容を文字列として取得
+
+
+        double height,weight,bmi;
+        height = Double.parseDouble(strHeight);
+        weight = Double.parseDouble(strWeight);
+        //　文字列として取得したものから数値を取り出す
+
+        height /= 100;
+        bmi = weight/(height * height);
+        String msg = "BMIスコア" + String.format("%.1f",bmi);
+
+        bmiScore.setText(msg);
+        // bmiScore.setText(String.format("%.1f",msg));
     }
 }
